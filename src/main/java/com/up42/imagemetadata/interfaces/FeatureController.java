3787437ffccabc4049e92a8dd3a31f8a830766ad
aligolgol <golgol.postbox@gmail.com>
2021-12-19 15:28:02 +0330

    Get request to get feature by id

--- src/main/java/com/up42/imagemetadata/interfaces/FeatureController.java
+++ src/main/java/com/up42/imagemetadata/interfaces/FeatureController.java
@@ -4,6 +4,7 @@ import com.up42.imagemetadata.application.dtos.FeatureDTO;
 import com.up42.imagemetadata.application.queryservices.GetFeaturesQueryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +22,10 @@ public class FeatureController {
     @GetMapping(value = "/features")
     public ResponseEntity<List<FeatureDTO>> getAll(){
         return ResponseEntity.ok(queryService.getFeatures());
+    }
+
+    @GetMapping(value = "/features/{id}")
+    public ResponseEntity<FeatureDTO> getById(@PathVariable("id") String id){
+        return ResponseEntity.ok(queryService.getFeatureById(id));
     }
 }
--- src/test/java/com/up42/imagemetadata/interfaces/FeatureControllerTest.java
+++ src/test/java/com/up42/imagemetadata/interfaces/FeatureControllerTest.java
@@ -3,6 +3,7 @@ package com.up42.imagemetadata.interfaces;
 import com.google.gson.Gson;
 import com.up42.imagemetadata.application.dtos.FeatureDTO;
 import com.up42.imagemetadata.application.queryservices.GetFeaturesQueryService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -25,20 +26,43 @@ public class FeatureControllerTest {
     GetFeaturesQueryService queryService;
     @Autowired
     private MockMvc mockMvc;
+    Gson gson;
+
+    @BeforeEach
+    void setup() {
+        gson = new Gson();
+    }
 
     @Test
     void should_return_all_features() throws Exception {
-        Gson gson = new Gson();
-        var featureDTO = List.of(FeatureDTO.builder()
+        var featureDTOs = List.of(FeatureDTO.builder()
                 .id("39c2f29e-c0f8-4a39-a98b-deed547d6aea")
                 .beginViewingDate(1554831167697L)
                 .endViewingDate(1554831202043L)
                 .timestamp(1554831167697L)
                 .missionName("Sentinel-1B").build());
-        when(queryService.getFeatures()).thenReturn(featureDTO);
+        when(queryService.getFeatures()).thenReturn(featureDTOs);
 
         mockMvc.perform(get(API_BASE_PATH)
                         .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(gson.toJson(featureDTOs)));
+    }
+
+    @Test
+    void should_return_the_feature_by_id() throws Exception {
+        var featureId = "39c2f29e-c0f8-4a39-a98b-deed547d6aea";
+        var featureDTO = FeatureDTO.builder()
+                .id(featureId)
+                .beginViewingDate(1554831167697L)
+                .endViewingDate(1554831202043L)
+                .timestamp(1554831167697L)
+                .missionName("Sentinel-1B").build();
+
+        when(queryService.getFeatureById(featureId))
+                .thenReturn(featureDTO);
+
+        mockMvc.perform(get(API_BASE_PATH + "/" + featureId).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().json(gson.toJson(featureDTO)));
     }
