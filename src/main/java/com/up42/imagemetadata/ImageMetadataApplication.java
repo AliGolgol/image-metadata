e3c372ec60932e1e3009d3f82f800e43c82210cb
aligolgol <golgol.postbox@gmail.com>
2021-12-19 16:54:09 +0330

    Adding swagger

--- src/main/java/com/up42/imagemetadata/ImageMetadataApplication.java
+++ src/main/java/com/up42/imagemetadata/ImageMetadataApplication.java
@@ -1,8 +1,11 @@
 package com.up42.imagemetadata;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(version = "V1.0.0", title = "image-metadata Service API"))
 @SpringBootApplication
 public class ImageMetadataApplication {
 
